f1605a3900670ce32f2ba1bcd386efb22885956d
work <Hosamksbaa@gmail.com>
2022-12-20 13:02:34 +0200

    ft

--- Fibonacci sequence.py
+++ Fibonacci sequence.py
@@ -7,11 +7,15 @@ n1, n2 = 0, 1
 count = 0
 
 
-print("Fibonacci sequence:")
-while count < nterms:
-    print(n1)
-    nth = n1 + n2
-    # update values
-    n1 = n2
-    n2 = nth
-    count += 1
+if nterms <= 0:
+   print("Please enter a positive integer")
+# if there is only one term, return n1
+else:
+    print("Fibonacci sequence:")
+    while count < nterms:
+        print(n1)
+        nth = n1 + n2
+        # update values
+        n1 = n2
+        n2 = nth
+        count += 1
