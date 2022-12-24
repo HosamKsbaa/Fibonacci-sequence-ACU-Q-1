bf6edcf1d03f98da9430cb46c9441ba69df397bc
Your Name <you@example.com>
2022-12-24 20:17:00 +0200

    cretae my branch

--- Fibonacci sequence.py
+++ Fibonacci sequence.py
@@ -8,7 +8,7 @@ count = 0
 
 
 if nterms <= 0:
-   print("Please enter a positive integer")
+    print("Please enter a positive integer")
 # if there is only one term, return n1
 else:
     print("Fibonacci sequence:")
@@ -19,4 +19,3 @@ else:
         n1 = n2
         n2 = nth
         count += 1
- 
